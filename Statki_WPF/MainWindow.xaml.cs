4a5f0f2dd11e76255f40b769aa9d9432da1e611c
W0RT4 <kstaniszewski@interia.eu>
2016-10-31 23:15:33 +0100

    dodane generowanie w wpf

--- Statki_WPF/MainWindow.xaml.cs
+++ Statki_WPF/MainWindow.xaml.cs
@@ -21,11 +21,79 @@ namespace Statki_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        static ImageSource imgWater = new BitmapImage(new Uri("water.gif", UriKind.RelativeOrAbsolute));
+        static ImageSource imgHit = new BitmapImage(new Uri("ogień.gif", UriKind.RelativeOrAbsolute));
+        static ImageSource imgMiss = new BitmapImage(new Uri("miss.jpg", UriKind.RelativeOrAbsolute));
+        public Image[,] gameBoard = new Image[10, 10];
+        public Image[,] gameBoard1 = new Image[10, 10];
         public MainWindow()
         {
             InitializeComponent();
-           
+            
+       
+         
+       
            
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow1.Height = 600;
+            MainWindow1.Width = 800;
+            GridStart.Visibility = Visibility.Hidden;
+            Grid1.Visibility = Visibility.Visible;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    gameBoard[i, j] = new Image
+                    {
+                        Source = imgWater,
+                        Stretch = Stretch.Fill
+                    };
+                    Grid.SetColumn(gameBoard[i, j], i);
+                    Grid.SetRow(gameBoard[i, j], j + 1);
+                    Grid1.Children.Add(gameBoard[i, j]);
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    gameBoard1[i, j] = new Image
+                    {
+                        Source = imgWater,
+                        Stretch = Stretch.Fill
+                    };
+                    Grid.SetColumn(gameBoard1[i, j], i + 11);
+                    Grid.SetRow(gameBoard1[i, j], j + 1);
+                    Grid1.Children.Add(gameBoard1[i, j]);
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow1.Height = 600;
+            MainWindow1.Width = 600;
+            GridStart.Visibility = Visibility.Hidden;
+           PlaceShip.Visibility = Visibility.Visible;
+           for (int i = 0; i < 10; i++)
+           {
+               for (int j = 0; j < 10; j++)
+               {
+                   gameBoard[i, j] = new Image
+                   {
+                       Source = imgWater,
+                       Stretch = Stretch.Fill
+                   };
+                   Grid.SetColumn(gameBoard[i, j], i+1);
+                   Grid.SetRow(gameBoard[i, j], j +1);
+                   PlaceShip.Children.Add(gameBoard[i, j]);
+               }
+           }
+        }
+      
     }
 }
